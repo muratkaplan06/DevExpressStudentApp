64ee60380befe2d79be3a11ba2d45e0da1076a91
Murat Kaplan <muratkaplan061989@gmail.com>
2024-03-27 16:26:05 +0300

    Add project files.

---
+++ StudentApp/XtraHome.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace StudentApp
+{
+    partial class XtraHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XtraHome));
+            pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            txtUserName = new DevExpress.XtraEditors.TextEdit();
+            txtPassword = new DevExpress.XtraEditors.TextEdit();
+            lblName = new Label();
+            label1 = new Label();
+            btnLogin = new DevExpress.XtraEditors.SimpleButton();
+            simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)pictureEdit1.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)txtUserName.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)txtPassword.Properties).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureEdit1
+            // 
+            pictureEdit1.EditValue = resources.GetObject("pictureEdit1.EditValue");
+            pictureEdit1.Location = new Point(203, 56);
+            pictureEdit1.Name = "pictureEdit1";
+            pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Zoom;
+            pictureEdit1.Size = new Size(251, 213);
+            pictureEdit1.TabIndex = 0;
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(203, 302);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(251, 20);
+            txtUserName.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(203, 346);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(251, 20);
+            txtPassword.TabIndex = 2;
+            
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lblName.ForeColor = Color.Black;
+            lblName.Location = new Point(203, 281);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(87, 18);
+            lblName.TabIndex = 3;
+            lblName.Text = "User Name";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(203, 325);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 18);
+            label1.TabIndex = 4;
+            label1.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Appearance.BackColor = Color.MidnightBlue;
+            btnLogin.Appearance.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLogin.Appearance.Options.UseBackColor = true;
+            btnLogin.Appearance.Options.UseFont = true;
+            btnLogin.Location = new Point(203, 393);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(251, 31);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // simpleButton1
+            // 
+            simpleButton1.Appearance.BackColor = Color.Transparent;
+            simpleButton1.Appearance.Options.UseBackColor = true;
+            simpleButton1.ImageOptions.Image = (Image)resources.GetObject("simpleButton1.ImageOptions.Image");
+            simpleButton1.Location = new Point(605, 3);
+            simpleButton1.Name = "simpleButton1";
+            simpleButton1.Size = new Size(39, 42);
+            simpleButton1.TabIndex = 8;
+            simpleButton1.Click += simpleButton1_Click;
+            // 
+            // simpleButton2
+            // 
+            simpleButton2.ImageOptions.Image = (Image)resources.GetObject("simpleButton2.ImageOptions.Image");
+            simpleButton2.Location = new Point(562, 3);
+            simpleButton2.Name = "simpleButton2";
+            simpleButton2.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            simpleButton2.Size = new Size(37, 42);
+            simpleButton2.TabIndex = 9;
+            simpleButton2.Click += simpleButton2_Click;
+            // 
+            // XtraHome
+            // 
+            Appearance.BackColor = Color.White;
+            Appearance.ForeColor = Color.Black;
+            Appearance.Options.UseBackColor = true;
+            Appearance.Options.UseForeColor = true;
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(669, 508);
+            Controls.Add(simpleButton2);
+            Controls.Add(simpleButton1);
+            Controls.Add(btnLogin);
+            Controls.Add(label1);
+            Controls.Add(lblName);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUserName);
+            Controls.Add(pictureEdit1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "XtraHome";
+            Text = "Student App";
+            ((System.ComponentModel.ISupportInitialize)pictureEdit1.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)txtUserName.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)txtPassword.Properties).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.XtraEditors.TextEdit txtUserName;
+        private DevExpress.XtraEditors.TextEdit txtPassword;
+        private Label lblName;
+        private Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnLogin;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+    }
+}
---
+++ StudentApp/XtraHome.cs
@@ -0,0 +1,32 @@
+﻿namespace StudentApp
+{
+    public partial class XtraHome : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraHome()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+        }
+    }
+}
